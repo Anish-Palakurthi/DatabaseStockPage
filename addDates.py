260389d58a39156ed6373c09190ab98c83a452d3
Anish <apalakurthi@icloud.com>
2021-08-09 11:21:08 -0500

    1121

--- addDates.py
+++ addDates.py
@@ -11,6 +11,7 @@ closes = []
 today = date.today()
 print(today)
 
+
 "http://api.marketstack.com/v1/eod?access_key=469ed3642bddff1dee77e5b1332ce3b7&symbols=AAPL&date_from=&date_to="
 
 
@@ -100,9 +101,10 @@ cnx = mysql.connector.connect(user='root', password='',
 # prepare a cursor object using cursor() method
 cursor = cnx.cursor()
 
+
 for i in range(len(dates)):
     message = (
-        "INSERT INTO `stocks2` (`ticker`, `dateOfPrice`, `price`) VALUES ('AAPL', '{day}', {price});").format(day=dates[i], price=closes[i])
+        "INSERT INTO stocks2(ticker, dateOfPrice, price) VALUES ('AAPL', '{day}', {price});").format(day=dates[i], price=closes[i])
     cursor.execute(message)
 
 
