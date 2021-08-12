f997a0d8637f91de9e4aa89555339e7d2b007502
Anish <apalakurthi@icloud.com>
2021-08-12 10:48:10 -0500

    prior to using flask

--- index.js
+++ index.js
@@ -1,7 +1,29 @@
+console.log("testing");
 const mysql = require("mysql2/promise");
+console.log("testing2")
+var dates = ["test1"];
+var prices1 = ["test2"];
 
+var prices2 = ["test3"];
 
 connect();
+
+function returnDates() {
+    console.log(dates)
+    return dates;
+}
+
+function returnPrices1() {
+    return prices1;
+}
+
+function returnPrices2() {
+    return prices2;
+}
+returnDates();
+returnPrices1();
+returnPrices2();
+
 async function connect() {
 
     try {
@@ -12,13 +34,13 @@ async function connect() {
             "database": "test.db"
 
         });
-        var dates = [];
+
 
 
         const q1 = await con.query(`SELECT * FROM stocks2 WHERE ticker = 'AAPL';`);
         var dateList = q1[0];
 
-        var prices1 = [];
+
 
 
         console.table(dateList);
@@ -32,25 +54,20 @@ async function connect() {
             var b = dateList[i].price;
             prices1.push(b);
 
-            console.log(dates[i]);
-            console.log(prices1[i]);
+            // console.log(dates[i]);
+            //console.log(prices1[i]);
 
         }
 
         const q2 = await con.query(`SELECT * FROM stocks2 WHERE ticker = 'TSLA';`);
         var dateList = q2[0];
 
-        var prices2 = [];
-
-
-        console.table(dateList);
-
 
         for (var i = 0; i < dateList.length; i++) {
 
             var b = dateList[i].price;
             prices2.push(b);
-            console.log(prices2[i]);
+            //console.log(prices2[i]);
 
         }
 
@@ -60,4 +77,9 @@ async function connect() {
         console.error(err);
     }
 
+
+
 }
+
+
+
